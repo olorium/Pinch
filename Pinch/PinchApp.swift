b071e876e414f62656f97cbbd9908ea1ab467a8f
Alex Vasyliev <olorin2007@gmail.com>
2023-04-06 13:15:51 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Oleksii Vasyliev on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
